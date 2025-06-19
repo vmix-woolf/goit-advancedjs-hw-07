296cb24bdb046db602f2e7094a68ef69a6cad1b3
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2025-06-19 22:03:58 +0300

    add task 2 and 3

--- src/3.ts
+++ src/3.ts
@@ -1,13 +1,14 @@
-/*
-  Ви створюєте гру, де є персонажі з різними ролями.
-  Зараз ви працюєте над класом Wizard, який має реалізовувати два інтерфейси - ICharacter та ISpellCaster.
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+  levelUp(): void;
+}
 
-  Визначте інтерфейси ICharacter та ISpellCaster так, щоб вони відповідали вимогам класу Wizard. 
-  Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp. 
-  Інтерфейс ISpellCaster повинен включати метод castSpell.
-*/
+interface ISpellCaster {
+  castSpell(): void;
+}
 
-// реалізація класу Wizard
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
     if (this.level < 1) {
@@ -29,11 +30,11 @@ class Wizard implements ICharacter, ISpellCaster {
   }
 }
 
-// тестування класу
+// Тестування класу
 const wizard = new Wizard('Merlin', 15);
 
-wizard.introduce('I am the mighty wizard');
-wizard.castSpell();
+wizard.introduce('I am the mighty wizard'); // I am the mighty wizard, Merlin
+wizard.castSpell(); // Casting a spell, behold my power!
 wizard.levelUp();  // Level up! New level is 16
 
 export {};
